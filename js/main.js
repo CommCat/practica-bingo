dd6e96cd9dcadb96334a76b93353440ad58d1ffa
CommCat <commandantecat@gmail.com>
2022-01-27 17:54:44 +0100

    new version first commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,76 @@
+
 import "../css/style.css";
 
 import { shuffle } from "./utils";
 
-const cartonJugador1 = [];
-const cartonJugador2 = [];
 
-const numerosEnElBombo = [];
-const numerosQueHanSalido = [];
+//
+const computersBingoCard = [];
+const calledNumbers = [];
+//const allNums = [];
 
-function crearCarton() {
-  const allNums = Array(90)
-    .fill(0)
-    .map((x, i) => i + 1);
+//function createCard() {
+  //creates a new array with 90 undefined elements
+  function range(start, end){
+    return Array(end - start + 1).fill(0).map((x, i) => i + 1);
+  }
+  const allNums = range(1, 90)
+  console.log(allNums)
 
+  
+ function createPlayersCard(){
   return shuffle(allNums).slice(0, 15);
-}
+ }
+console.log(createPlayersCard());
 
-function pintarCartonEnDOM(arrayDeNumeros) {
+function createComputersCard(){
+  return shuffle(allNums).slice(30, 45);
+ }
+console.log(createComputersCard());
+
+
+
+ //console.log(allNums);
+
+
+ //const playersBingoCard = [allNums.slice(0, 15)]
+ //console.log(playersBingoCard)
+    
+  //create new array of first 15 numbers
+ // return shuffle(allNums).slice(0, 15);
+  
+
+  //const playersBingoCard = [allNums.slice(0, 15)]
+  //console.log(playersBingoCard);
+
+
+
+/*
+function addNumbersToCard(arrayNumbers) {
   // aqui gestionamos la logica de pintar el HTML en el DOM
 }
 
-function pintarNumeroEnDOM(numero) {}
+function changeNumberColour(numero) {}
 
-function sacarNumeroDelBombo() {}
 
-function comprobarGanador(cartonJugador, numerosQueHanSalido) {}
+function drawNumber() {
+    const randomNumber = Math.floor(Math.random() * 90) + 1;
+    calledNumbers.push(randomNumber);
+    const showNumber = document.getElementById("showNumber");
+    showNumber.innerHTML = randomNumber
+}
 
+const draw = document.getElementById("button");
+
+button.addEventListener("click", function(){
+    const draw = drawNumber();
+    return draw;
+    
+    });
+
+    console.log(calledNumbers)
+
+
+
+function checkWinner(bingoCard, calledNumbers) {}
+*/
